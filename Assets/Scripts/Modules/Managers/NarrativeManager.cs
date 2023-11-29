609717f3bab569a0ac1befaee5203bbe3ccf6fbf
Psalms77 <pc4499520@gmail.com>
2023-11-28 16:00:18 -0800

    na setups

--- Assets/Scripts/Modules/Managers/NarrativeManager.cs
+++ Assets/Scripts/Modules/Managers/NarrativeManager.cs
@@ -7,14 +7,7 @@ public class NarrativeManager : Singleton<NarrativeManager>
     private GameObject narrativeManager;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
         
     }
